1c2b7f1aaff383220ee6c602ff47c2317a41d345
hayfa safa <haifasafaa@gmail.com>
2026-02-01 23:56:49 +0100

    test  http://localhost:4000/api/v1/users/logout works

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -65,14 +65,14 @@ const logoutUser = async (req, res) => {
   try {
 const {email} = req.body;
 
-const user = await user.findOne ({
+const user = await User.findOne ({
   email
 });
-if (!user) return res.status(404).json({
-  message: "user not found"
+if (!user) 
+  { return res.status(404).json({message: "user not found"
 }); 
-res.status(200).json({
-  message: "logout successful"
+}
+res.status(200).json({ message: "logout successful"
 }); 
 
  
@@ -80,7 +80,7 @@ res.status(200).json({
   catch(error) {
 res.status(500).json({
   message: "Internal Server Error", error : error.message
-})
+});
   }
 }
 
--- backend/src/app.js
+++ backend/src/app.js
@@ -14,5 +14,6 @@ app.use("/api/v1/users", userRouter);
 
 //route: http://localhost:4000/api/v1/users/register
 // route: http://localhost:4000/api/v1/users/login
+// route: http://localhost:4000/api/v1/users/logout
 
 export default app; 
