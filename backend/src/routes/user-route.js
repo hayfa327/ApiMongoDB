8db3f43a33317e22fac05a946bd905b19da99c4b
hayfa safa <haifasafaa@gmail.com>
2026-02-01 22:55:19 +0100

    create route.post for the login

--- backend/src/routes/user-route.js
+++ backend/src/routes/user-route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { registerUser } from "../../controllers/user.controllers.js";
+import { loginUser } from "../../controllers/user.controllers.js";
 
 const router = Router(); 
 
 
 
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
 export default router; 
