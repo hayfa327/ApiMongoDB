cdc75efb2f3bf30e30cda49ad85d25cdc464bdc7
hayfa safa <haifasafaa@gmail.com>
2026-02-01 22:35:59 +0100

    install bcrypt and create hash function

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,6 @@
  
 import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema (
   {
@@ -31,8 +32,17 @@ email: {
   Timestamps : true
   }
 
- 
 )
+
+//  before saving any password we need to hash it 
+userSchema.pre("save" , async function(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+
+  next(); 
+});
+
+
 
 
 export const User = mongoose.model("User", userSchema); 
