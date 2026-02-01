d194870df4cca2bf74634d887d62ee28dfee27f7
hayfa safa <haifasafaa@gmail.com>
2026-02-01 23:18:05 +0100

    test http://localhost:4000/api/v1/users/login works

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -36,10 +36,9 @@ email: {
 
 //  before saving any password we need to hash it 
 userSchema.pre("save" , async function(next) {
-  if (!this.isModified("password")) return next();
+  if (!this.isModified("password")) return ;
   this.password = await bcrypt.hash(this.password, 10);
 
-  next(); 
 });
 
 //compare passwords
--- backend/src/app.js
+++ backend/src/app.js
@@ -13,5 +13,6 @@ app.use("/api/v1/users", userRouter);
  
 //example
 //route: http://localhost:4000/api/v1/users/register
+// route: http://localhost:4000/api/v1/users/login
 
 export default app; 
