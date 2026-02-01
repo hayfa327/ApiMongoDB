a92af9448d239c2724029f6a4c2da0b2f7a3ca14
hayfa safa <haifasafaa@gmail.com>
2026-02-01 22:39:55 +0100

    add compare password function

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -42,7 +42,10 @@ userSchema.pre("save" , async function(next) {
   next(); 
 });
 
-
+//compare passwords
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
 
 
 export const User = mongoose.model("User", userSchema); 
