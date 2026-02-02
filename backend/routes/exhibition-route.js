c2f6241b9e16ae1e37abed23eb0bd56218e20134
hayfa safa <haifasafaa@gmail.com>
2026-02-03 00:34:10 +0100

    add exibition route.js

---
+++ backend/routes/exhibition-route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+ import { createExibition } from '../controllers/exhibition.controller';
+ import { auth } from '../middleware/auth.js';
+ import { isAdmin } from '../middleware/auth.js';
+
+const router = express.Router();
+
+
+
+router.post('/create', auth, isAdmin, createExibition);
+
+export default router;
--- backend/src/app.js
+++ backend/src/app.js
@@ -6,15 +6,18 @@ app.use(express.json());
 
 //import routes
 import userRouter from "../routes/user-route.js";
+import exhibitionRouter from "../routes/exhibition-route.js";
  
 //route declaration
 app.use("/api/v1/users", userRouter); 
- 
- 
+
 
 //route: http://localhost:4000/api/v1/users/register
 // route: http://localhost:4000/api/v1/users/login
 // route: http://localhost:4000/api/v1/users/logout
 // route: http://localhost:4000/api/v1/users/change-password
+
+
+app.use("/api/v1/exhibitions", exhibitionRouter);
 
 export default app; 
