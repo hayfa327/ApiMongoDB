21f9ac7a6ac43cc355b3ea59c26cac038112b84c
hayfa safa <haifasafaa@gmail.com>
2026-02-02 18:05:46 +0100

    add file middleware for help change password function

--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
 import { loginUser , logoutUser} from "../controllers/user.controllers.js";
+
+import { auth } from "../middleware/auth.js";
   
 
 const router = Router(); 
@@ -10,5 +12,6 @@ const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(logoutUser);
+router.put("/change-password", auth, changePassword)
 
 export default router; 
