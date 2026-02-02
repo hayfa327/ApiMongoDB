d96f426bf339bed25d249bb7c75389d37f887186
hayfa safa <haifasafaa@gmail.com>
2026-02-02 17:20:19 +0100

    thr routs file in backend file

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 //import routes
-import userRouter from "./routes/user-route.js";
+import userRouter from "../routes/user-route.js";
  
 //route declaration
 app.use("/api/v1/users", userRouter); 
