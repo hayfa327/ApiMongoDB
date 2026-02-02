23b769ee249cbdb17b63ce9fe6569a480004029b
hayfa safa <haifasafaa@gmail.com>
2026-02-02 22:38:10 +0100

    add isAmin in auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -20,8 +20,18 @@ import jwt from "jsonwebtoken"
   }
 };
 
+const isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({message: "Access denied . Admin only"
+  });
+}
+next();
+};
+
+
 
 
 export {
-  auth
+  auth, 
+  isAdmin 
 }
