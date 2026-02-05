bce3c6f2ae8062e858939b7e1824ae1917b3df7e
hayfa safa <haifasafaa@gmail.com>
2026-02-05 21:57:31 +0100

    delete old code

--- backend/src/database.js
+++ backend/src/database.js
@@ -18,37 +18,3 @@ console.log("MongoDB connection failed", error.message);
 
 export default connectDB;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const connectDB = async () => {
-  try {
-    const connectioInstance = await mongoose.connect 
-    (`${process.env.MONGODB_URI}`)
-    console.log(`\n MongoDB connected !!!  
-    const connectioInstance = await mongoose.connect 
-    ${connectioInstance.connection.host}`);
-
-  } catch (error) {
-  console.log("MongoDB connection failed, error"); 
-  process.exit(1)
-  }
-}
-
-export default connectDB;'*/
