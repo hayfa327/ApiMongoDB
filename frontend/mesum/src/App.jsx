95d335a11b87cf3ee089a968bda5f56f9728f39e
hayfa safa <haifasafaa@gmail.com>
2026-03-16 23:53:40 +0100

    check up

--- frontend/mesum/src/App.jsx
+++ frontend/mesum/src/App.jsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
     <main>
-      
+
     </main>
   )
 }
+
+
+/* npm install framer-motion
+npm install locomotive-scroll
+npm install three
+npm install @react-three/fiber
+npm install @react-three/drei */
