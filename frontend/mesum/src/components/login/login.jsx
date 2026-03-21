05a0efbf210a7e907a97d17631d7f7f4a1a76532
hayfa safa <haifasafaa@gmail.com>
2026-03-21 14:14:01 +0100

    add the login function

--- frontend/mesum/src/components/login/login.jsx
+++ frontend/mesum/src/components/login/login.jsx
@@ -1,16 +1,78 @@
+import {useState} from "react"
+import login from "../../assets/login.png"
+import "./login.css"
+
+ 
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (loginFunction) => {
+    loginFunction.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://mesum-api.onrender.com/api/v1/users/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert(data.message);
+        return;
+      }
+
+      console.log("SUCCESS:", data);
+ 
+      localStorage.setItem("token", data.token);
+
+      alert("Login successful ");
+
+    } catch (error) {
+      console.error(error);
+      alert("Something went wrong");
+    }
+  };
+
   return (
-  <section> 
-     <img src="../../assets/photo.jpg" alt="" /> 
-     <h1>Welcome Back</h1>
-     <p>Log in to your account</p>
-     <form>
-      <input type="email" />
-      <input type="text" />
-      <button>Log In</button>
-     </form>
-     <p>Dont't have an account? <span>Sign Up</span></p>
-     </section>
-    
-  )
+    <section className="loginPart">
+      <img src={login} alt="login visual" />
+
+      <h1>Welcome Back</h1>
+      <p>Log in to your account</p>
+
+      <form className="loginForm" onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email@example.com"
+          value={email}
+          onChange={(loginValue) => setEmail(loginValue.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(loginValue) => setPassword(loginValue.target.value)}
+        />
+
+        <button type="submit">Log In</button>
+      </form>
+
+      <p>
+        Don't have an account? <span>Sign Up</span>
+      </p>
+    </section>
+  );
 }
