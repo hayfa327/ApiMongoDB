25572d7b0bedfd4a7ffbab627fa34392d5a7eb1d
hayfa safa <haifasafaa@gmail.com>
2026-03-30 14:33:17 +0200

    add the console.log im backend

--- backend/src/database.js
+++ backend/src/database.js
@@ -3,13 +3,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+     console.log("MONGO URI:", process.env.MONGODB_URI);
 const connectionInstance = await mongoose.connect(
    process.env.MONGODB_URI);
 
 
    console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
 
-
+  console.log(`Database name: ${connectionInstance.connection.name}`);
   }
   catch (error) {
 console.log("MongoDB connection failed", error.message);
