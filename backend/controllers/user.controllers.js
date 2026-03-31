5f580b0774168c6fe8c0117acb96ca66965b3750
hayfa safa <haifasafaa@gmail.com>
2026-03-31 02:06:13 +0200

    test for the get artist

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -126,7 +126,7 @@ res.status(500).json({message: "Internal Server Error", error: error.message});
 const getAllArtists = async (req, res) => {
   try {
     const artists = await User.find({ role: "artist" })
-      .select("username email role");
+      .select("_id username email role");
 
     res.status(200).json({ artists });
 
