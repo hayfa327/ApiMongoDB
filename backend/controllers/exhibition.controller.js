26c083c023cc154c4aa539f457786170bf1da3aa
hayfa safa <haifasafaa@gmail.com>
2026-04-27 00:20:21 +0200

    change the place for function order

--- backend/controllers/exhibition.controller.js
+++ backend/controllers/exhibition.controller.js
@@ -48,6 +48,26 @@ const getAllExhibitions = async (req, res) => {
 }
 
 
+const getExhibitionsByArtist = async (req, res) => {
+  try {
+    const exhibitions = await Exhibition.find({
+      artist: req.params.artistId
+    })
+      .populate("artist", "username email")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({ exhibitions });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching exhibitions by artist",
+      error: error.message
+    });
+  }
+};
+
+
+
 const getExhibitionById = async (req, res) => {
   try {
     const { id } = req.params;
@@ -71,29 +91,13 @@ const getExhibitionById = async (req, res) => {
 };
 
 
-const getExhibitionsByArtist = async (req, res) => {
-  try {
-    const exhibitions = await Exhibition.find({
-      artist: req.params.artistId
-    })
-      .populate("artist", "username email")
-      .sort({ createdAt: -1 });
-
-    res.status(200).json({ exhibitions });
-
-  } catch (error) {
-    res.status(500).json({
-      message: "Error fetching exhibitions by artist",
-      error: error.message
-    });
-  }
-};
+ 
 
 
 
 export {
   createExibition,
   getAllExhibitions, 
+   getExhibitionsByArtist,
   getExhibitionById,
-  getExhibitionsByArtist
 }
