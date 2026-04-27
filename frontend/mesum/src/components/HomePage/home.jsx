66c455de7f6ef9263d4bf8c2055b144a6b314b7c
hayfa safa <haifasafaa@gmail.com>
2026-04-27 23:09:13 +0200

    test the phone version

--- frontend/mesum/src/components/HomePage/home.jsx
+++ frontend/mesum/src/components/HomePage/home.jsx
@@ -1,6 +1,6 @@
 import Hero from "./heroSection/heroSection"
 import HomeExhibition from "./exhibitions/homeExhibitions"
-import NewsletterFooter from "../footer/footer" 
+
 
 
 export default function Home() {
@@ -10,7 +10,6 @@ export default function Home() {
     <>
       <Hero />
       < HomeExhibition />
-     < NewsletterFooter />
        
     </>
   );
